562d7198579b5eeb016713232571a4d38f57c570
DonaLd-D <57483340+DonaLd-D@users.noreply.github.com>
2020-10-17 17:20:11 +0800

    URL Mapping

--- src/main/java/com/imooc/springmvc/controller/URLMappingController.java
+++ src/main/java/com/imooc/springmvc/controller/URLMappingController.java
@@ -1,20 +1,30 @@
 package com.imooc.springmvc.controller;
 
+import com.imooc.springmvc.entity.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/um")
 public class URLMappingController {
     @GetMapping("/g")
+//    @RequestMapping(value = "/g",method = RequestMethod.GET)
     @ResponseBody
-    public String getMapping(){
+    public String getMapping(@RequestParam("manager_name") String abc){
+        System.out.println("manager_name:"+abc);
         return "this is get method";
     }
     @PostMapping("/p")
     @ResponseBody
-    public String postMapping(){
+    public String postMapping(String username,Long password){
+        System.out.println(username+":"+password);
         return "this is post method";
+    }
+
+    @PostMapping("/p1")
+    @ResponseBody
+    public String postMapping1(User user){
+        System.out.println(user.getUsername()+":"+user.getPassword());
+        return "This is post method";
     }
 }
